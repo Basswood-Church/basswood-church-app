f00fefd6069c7854239192bfe01b54b17260b948
Nathaniel Bond <nabond251@gmail.com>
2020-10-29 00:42:16 -0400

    Test plan scrolls to current date

--- test/bible_reading_plan_widget_test.dart
+++ test/bible_reading_plan_widget_test.dart
@@ -17,4 +17,62 @@ void main() {
     // Verify that our widget starts with a loading indicator.
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
+
+  testWidgets('Bible reading plan scrolls to current day', (WidgetTester tester) async {
+    final DateTime now = DateTime.now();
+    final DateTime today = DateTime(now.year, now.month, now.day);
+    const String past = 'past';
+    const String friendlyPast = 'Past Passage';
+    const String present = 'present';
+    const String friendlyPresent = 'Present Passage';
+    const String future = 'future';
+    const String friendlyFuture = 'Future Passage';
+
+    final List<BrcDay> testData = <BrcDay>[
+      BrcDay(
+          date: today.subtract(const Duration(days: 2)),
+          passage: past,
+          friendlyPassage: friendlyPast),
+      BrcDay(
+          date: today.subtract(const Duration(days: 1)),
+          passage: past,
+          friendlyPassage: friendlyPast),
+      BrcDay(
+          date: today,
+          passage: present,
+          friendlyPassage: friendlyPresent),
+      BrcDay(
+          date: today.add(const Duration(days: 1)),
+          passage: future,
+          friendlyPassage: friendlyFuture),
+      BrcDay(
+          date: today.add(const Duration(days: 2)),
+          passage: future,
+          friendlyPassage: friendlyFuture),
+      BrcDay(
+          date: today.add(const Duration(days: 3)),
+          passage: future,
+          friendlyPassage: friendlyFuture),
+      BrcDay(
+          date: today.add(const Duration(days: 4)),
+          passage: future,
+          friendlyPassage: friendlyFuture),
+    ];
+
+    // Need to wrap stateful list widget in app/builder to have build context
+    await tester.pumpWidget(
+        MaterialApp(
+            home: Builder(
+                builder: (BuildContext context) => BrcDaysList(brcDays: testData)
+            )
+        )
+    );
+
+    // Let the animation finish
+    await tester.pumpAndSettle();
+
+    expect(find.text(friendlyPast), findsNothing);
+    expect(find.text(friendlyPresent), findsOneWidget);
+    expect(find.text(friendlyFuture), findsWidgets);
+  });
 }
